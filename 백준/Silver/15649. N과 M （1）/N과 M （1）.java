0d5c296c219645cd83fecc67ff16ab03de2c159c
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-06-18 20:02:38 +0900

    [Silver III] Title: N과 M (1), Time: 2188 ms, Memory: 61516 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int n, r;
+    static boolean[] visited;
+    static int[] answer;
+    static int[] result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] split = br.readLine().split(" ");
+        n = Integer.parseInt(split[0]);
+        r = Integer.parseInt(split[1]);
+
+        visited = new boolean[n];
+        answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = i + 1;
+        }
+        result = new int[r];
+        perm(0);
+
+    }
+
+    static void perm(int depth){
+        if (depth == r){
+            for (int i = 0; i < r; i++) {
+                System.out.print(result[i] + " ");
+            }
+            System.out.println();
+            return;
+        }else{
+            for (int i = 0; i < n; i++) {
+                if(!visited[i]){
+                    visited[i] = true;
+                    result[depth] = answer[i];
+                    perm(depth + 1);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+}
