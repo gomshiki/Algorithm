160fccc40b0c7f68b1e6408e45cad75bc56a858e
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-06-21 20:06:04 +0900

    [level 2] Title: 양궁대회, Time: 19.91 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,47 +1,83 @@
+import java.util.*;
+
 class Solution {
-    static private int[] res = new int[11];//점수차가 최대일때 라이언이 쏜 화살배열
-    static private int[] lion= {-1};//정답배열
-    static private int max = Integer.MIN_VALUE;//최대값
-    public static int[] solution(int n, int[] info) {
-        back(0,n,info);//라이언이 쏜 화살에 대한 조합
+    int[] ryan = new int[11];
+    int[] apeach = new int[11];
+    int maxPoint = 0; //라이언이 어피치보다 얻을 점수의 max값
+    ArrayList<int[]> combinations = new ArrayList<>();
 
-        if(max==-1) {//라이언이 어피치를 못 이길떄
-            lion = new int[1];
-            lion[0]=-1;
+    public int[] solution(int n, int[] info) {
+        int[] answer = {};
+
+        this.apeach = info; //변수명 맞춰주는 용도
+
+        /* 어피치 점수 구하기 */
+        int apeachScore = 0;
+        for(int i = 0; i < 11; i++) {
+            if (apeach[i] > 0) { apeachScore += (10-i); }
         }
-        return lion;
-    }
 
-    public static void back(int depth, int n, int[] info) {
-        //화살 다 꽂았을때(종료조건)
-        if(depth==n) {
-            int diff = score(info);//점수차 구하기
-            if(max<=diff) {//점수차 최대값 갱신
-                max = diff;
-                lion = res.clone();
+        /* maxPoint, combinations 구하기 */
+        maxPoint = -apeachScore;
+        permutation(0, n, maxPoint);
+
+        /* 조합 고르기 */
+        if (maxPoint <= 0) { answer = new int[] {-1}; }
+        else { //가장 낮은 점수를 더 많이 맞힌 조합 구하기
+            for (int i = 0; i <= 10; i++) { 
+                if(combinations.size() == 1) { break; }
+
+                /* 해당 점수를 맞힌 최대 개수 구하기 */
+                int maxCount = 0;
+                for (int[] ryan : combinations) {
+                    maxCount = Math.max(ryan[10-i], maxCount);
+                }
+
+                /* 최대 개수를 맞힌 조합만 남기기 */
+                int c = 0;
+                while(c < combinations.size()) {
+                    if(combinations.get(c)[10-i] != maxCount) { combinations.remove(c); }
+                    else { c++; }
+                }
             }
-            return;
+            answer = combinations.get(0);
         }
 
-        //res[i]<=info[i] -> i과녁에 라이언이 화살을 더 많이 맞추면 반복문 종료한다.
-        for(int i=0; i<info.length && res[i]<=info[i]; i++) {
-            res[i] += 1;
-            back(depth+1, n, info);
-            res[i] -= 1;
-        }
+        return answer;
     }
 
-    //점수차 구하기
-    public static int score(int[] info) {
-        int apeach=0, lion=0;
-        for(int i=0; i<res.length; i++) {
-            if(info[i]==0 && res[i]==0) continue;//i원소에 둘다 0개 맞췄을땐 무시.
-            if(info[i]>=res[i]) apeach += (10-i);
-            else lion += (10-i);
+    public void permutation(int index, int n, int nowPoint) { 
+        //index = ryan{과녁별 화살개수 배열} 인덱스; 
+        //n = 라이언이 쏠 수 있는 화살 개수; 
+        //nowPoint = 현재 라이언이 어피치보다 얻은 점수
+
+        /* 화살 모두 쏜 후, 이번 조합의 점수 확인 */
+        if (n == 0) {            
+            if(nowPoint > maxPoint) {
+                maxPoint = nowPoint;
+                combinations = new ArrayList<>();
+                combinations.add(ryan.clone());
+            } 
+            else if (nowPoint == maxPoint) {
+                combinations.add(ryan.clone());
+            }
         }
 
-        int diff = lion - apeach;
-        if(diff<=0) return -1;
-        return diff;
+        /* 라이언 {과녁별 화살개수} 조합 만들기 */
+        else if (n >= 0 && index < 11) {
+            for(int num = 0; num <= apeach[index] + 1; num++) {
+                ryan[index] += num; //화살 쏘기
+
+                int nextPoint = nowPoint;
+                if(ryan[index] > apeach[index]) { //점수를 라이언이 가져가는 상황
+                    if (apeach[index] == 0) { nextPoint += (10 - index); } //어피치가 안쏜 곳일 때
+                    else if (apeach[index] > 0) { nextPoint += (10 - index) * 2; } //어피치가 쏜 곳일 때
+                }
+
+                permutation(index+1, n-num, nextPoint);
+
+                ryan[index] -= num; //원상 복구
+            }
+        }
     }
 }
