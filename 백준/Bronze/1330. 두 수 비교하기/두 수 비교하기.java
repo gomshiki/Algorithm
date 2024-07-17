0ba358b587467fe7e335deddb0688a35816f6e52
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-07-17 10:31:22 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 108 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,29 +1,26 @@
-import java.io.*;
-import java.util.StringTokenizer;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        /** 스트림 : 입력장치와 프로그램 사이 단방향으로 연결해주는 역할 **/
-        /** InputStream :  입력받은 데이터를 바이트단위로 읽어드림(=Byte Stream) **/
-        /** InputStreamReader : 문자단위로 읽어드림 (=Character Stream) **/
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = array[0];
+        int b = array[1];
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-
-
-        if (a > b) {
-            System.out.println(">");
-        } else if (a < b) {
-            System.out.println("<");
-        } else {
-            System.out.println("==");
+        if(a > b){
+            sb.append(">");
+        }else if(a < b){
+            sb.append("<");
+        }else{
+            sb.append("==");
         }
 
-
+        System.out.println(sb.toString());
     }
+
 }
