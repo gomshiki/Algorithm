93dc64b16977d20a698258154a22599decdba6ed
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-07-18 10:11:52 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String[] split = br.readLine().split("");
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(split[j]);
+            }
+        }
+        turn();
+        debug(map);
+
+    }
+
+    static void turn() {
+        int[][] temp = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                temp[i][M - 1 - j] = map[i][j];
+            }
+        }
+
+        map = temp.clone();
+    }
+
+    static void debug(int[][] arr) {
+        for (int[] a : arr) {
+            for (int b : a) {
+                System.out.print(b);
+            }
+            System.out.println();
+        }
+    }
+}
