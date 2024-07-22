aa264f895a2ff93c8a4ab657bf49648eed1cbdf6
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-07-22 16:36:51 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+
+        //System.out.println(MenOfPassion(n));
+        //System.out.println(cnt);
+
+        System.out.println((n * (n - 1) * (n - 2)) / 6);
+        System.out.println(3);
+
+    }
+}
