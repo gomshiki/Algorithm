f51d11d5de8a1a3eb1d9e71bd3baaf3ab4c5b2ea
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-07-23 20:39:15 +0900

    [Silver III] Title: N과 M (1), Time: 364 ms, Memory: 25836 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,44 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
-
-    static int n, r;
+    static int[] arr, result;
     static boolean[] visited;
-    static int[] answer;
-    static int[] result;
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt(); // 뽑을 갯수
 
-        String[] split = br.readLine().split(" ");
-        n = Integer.parseInt(split[0]);
-        r = Integer.parseInt(split[1]);
-
-        visited = new boolean[n];
-        answer = new int[n];
-        for (int i = 0; i < n; i++) {
-            answer[i] = i + 1;
+        // 1 ~ N 까지 숫자를 배열에 저장
+        arr = new int[N];
+        result = new int[M];
+        visited = new boolean[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = i + 1;
         }
-        result = new int[r];
-        perm(0);
 
+        // 1, 2, 3, 4 중 2개만 선택
+        backTracking( 0);
+        System.out.println(sb);
     }
 
-    static void perm(int depth){
-        if (depth == r){
-            for (int i = 0; i < r; i++) {
-                System.out.print(result[i] + " ");
+    static void backTracking(int depth) {
+        if (depth == M) {// depth가 M이면 종료되어야함.
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }else{
-            for (int i = 0; i < n; i++) {
-                if(!visited[i]){
+            for (int i = 0; i < N; i++) {
+                if (!visited[i]) {
                     visited[i] = true;
-                    result[depth] = answer[i];
-                    perm(depth + 1);
+                    result[depth] = arr[i];
+                    backTracking(depth + 1);
                     visited[i] = false;
                 }
             }
