4df48d5cb5b9f172bf97fa5723f3f0794c3e2d91
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-07-27 10:49:20 +0900

    [Silver II] Title: 최소 힙, Time: 288 ms, Memory: 26528 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,29 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.PriorityQueue;
 
-public class Main {
+public class Main { // 최소 힙 : 부모노드가 자식노드보다 항상 작아야함. => 오름차순
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-
+        StringBuilder sb = new StringBuilder();
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-
         for (int i = 0; i < N; i++) {
-            int x = Integer.parseInt(br.readLine());
+            int parseInt = Integer.parseInt(br.readLine());
 
-            if (x == 0) {
+            if (parseInt > 0) {
+                pq.offer(parseInt);
+            }
+            if (parseInt == 0) {
                 if (pq.isEmpty()) {
-                    System.out.println(0);
+                    sb.append(0).append("\n");
                 }else{
-                    System.out.println(pq.poll());
+                    sb.append(pq.poll()).append("\n");
                 }
-            } else {
-                pq.add(x);
             }
         }
+
+        System.out.println(sb.toString());
     }
 }
